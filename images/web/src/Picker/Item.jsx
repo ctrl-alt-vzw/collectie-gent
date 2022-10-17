5e8960b28d6e9b131753720a8631c2cd776013b3
Jan Everaert <jan@crashlab.be>
2022-10-17 09:41:03 +0200

    fix: resolve web colors error

--- images/web/src/Picker/Item.jsx
+++ images/web/src/Picker/Item.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 function Item(props){
-  console.log(props)
+  console.log(props.data)
   const image = `https://api.collectie.gent/iiif/imageiiif/3/${props.data.gentImageURI}/full/^1000,/0/default.jpg`
   return( 
     <div className="item">
       <div className="imageContainer">
         <img src={image} alt={props.data.annotation} style={{
-          backgroundColor: `rgba(${ props.data.imagedata.colors ? props.data.imagedata.colors.tl.join(',') : "0,0,0,1"})`,
+          backgroundColor: `rgba(${ props.data.imagedata && props.data.imagedata.colors ? props.data.imagedata.colors.tl.join(',') : "0,0,0,1"})`,
           minHeight: '200px'
         }} />
       </div>
