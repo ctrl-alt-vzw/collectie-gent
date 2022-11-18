90a360a043807ed20263ab7d95270ad902fa3e92
Joni De Borger <joni.de.borger@ehb.be>
2022-11-18 17:55:14 +0100

    added visual to mechanical image

---
+++ images/mechanical/src/static/js/Vis.js
@@ -0,0 +1,105 @@
+"use strict";
+
+const INDEX_OFFSET = 1932
+const CANVAS_WIDTH = 1280;
+const CANVAS_HEIGHT = 1280;
+const ITEMS_AMOUNT = 48332;
+
+export default class Vis {
+    constructor() {
+        this.context = this.getContext();
+        this.reviewedItems = [];
+
+        this.render();
+        //fetch items
+        this.getReviewedItems();
+    }
+
+    getContext() {
+        let canvas = document.querySelector('canvas');
+        let parent = canvas.closest('div');
+        /* canvas.width = parent.clientWidth;
+        canvas.height = parent.clientHeight; */
+        canvas.width = CANVAS_HEIGHT;
+        canvas.height = CANVAS_HEIGHT;
+        return canvas.getContext('2d');
+    }
+
+    async getReviewedItems() {
+        const resp = await fetch('https://api.datacratie.cc/approvals');
+        this.reviewedItems = await resp.json();
+
+        this.renderLastItems();
+        this.renderPercentage();
+        this.render();
+    }
+
+    renderLastItems() {
+        const lastItems = this.reviewedItems.slice(this.reviewedItems.length - 6, this.reviewedItems.length - 1);
+        const table = document.getElementById('last-items');
+        lastItems.forEach((item, i) => {
+            const row = table.insertRow(i + 1);
+            row.insertCell(0).innerHTML = item.annotationUUID;
+            row.insertCell(1).innerHTML = item.collection;
+            row.insertCell(2).innerHTML = item.created_at;
+            row.insertCell(3).innerHTML = item.workerID;
+            if (item.approved) {
+                row.insertCell(4).innerHTML = "<div class='box red'></div>";
+            } else {
+                row.insertCell(4).innerHTML = "<div class='box green'></div>";
+            }
+
+        })
+    }
+
+    renderPercentage() {
+        document.getElementById('percentage').innerHTML = `${parseFloat(this.reviewedItems.length / ITEMS_AMOUNT)} %`;
+    }
+
+    render() {
+        this.context.lineWidth = 1;
+        this.context.fillStyle = '#000';
+        this.context.fillRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+        let cols = 220;
+        let rows = 220;
+        let size = 5;
+        let offset = 0;
+
+        let index = 0;
+        for (let j = 0; j < rows; j++) {
+            for (let i = 0; i < cols; i++) {
+                if (index < ITEMS_AMOUNT) {
+                    let item = this.getItem(index + INDEX_OFFSET);
+                    if (!item) {
+                        this.context.strokeStyle = '#000';
+                        this.context.strokeRect(offset + i * size, offset + j * size, size, size);
+                        this.context.fillStyle = 'rgba(125, 125, 125, 0.5)';
+                        this.context.fillRect(offset + i * size, offset + j * size, size, size);
+                    } else if (item.approved) {
+                        this.context.fillStyle = 'green';
+                        this.context.fillRect(offset + i * size, offset + j * size, size, size);
+                    } else if (!item.approved) {
+                        this.context.fillStyle = 'red';
+                        this.context.fillRect(offset + i * size, offset + j * size, size, size);
+
+                    }
+                }
+                index++;
+            }
+        }
+    }
+
+    getItem(index) {
+        let items = this.reviewedItems.filter(item => {
+            if (item.annotationUUID == index) {
+                return item
+            }
+        });
+
+        if (items.length) {
+            return items[0]
+        } else {
+            return null;
+        }
+    }
+}
--- images/mechanical/src/static/js/index.js
+++ images/mechanical/src/static/js/index.js
@@ -1,3 +1,5 @@
+import Vis from "./Vis.js";
+
 const BASE_URI = "https://api.datacratie.cc"
 const USE_SMALL_IMAGES = true;
 const SCROLLTIME = 200;
@@ -7,6 +9,8 @@ let currentlyWatching = {};
 const currentImages = [];
 let currentWorker = generateUUID();
 let workerHasWorked = 0;
+
+const visualisation = new Vis();
 
 function fetchImages() {
   fetch(BASE_URI + "/annotation/random")
@@ -18,16 +22,16 @@ function fetchImages() {
 }
 
 function renderLoop() {
-  if(scrolling) {
+  if (scrolling) {
     renderItem();
     currentImages.shift();
-    if(currentImages.length <= 3) { fetchImages()}
+    if (currentImages.length <= 3) {
+      fetchImages()
+    }
     const to = setTimeout(() => {
       renderLoop()
     }, SCROLLTIME);
-  }
-  else {
-  }
+  } else {}
 }
 
 
@@ -35,13 +39,13 @@ function renderItem() {
   const el = currentImages[0];
   currentlyWatching = el;
   let colorItems = ""
-  if(el) {
-    if( el.colordata && el.colordata.colors) {
+  if (el) {
+    if (el.colordata && el.colordata.colors) {
       el.colordata.colors.forEach((color) => {
         colorItems += `<div class="color" style="background: rgba(${color[0]}, ${color[1]}, ${color[2]}, 1);"></div>`
       })
-    }   
-    const htmlString =  `
+    }
+    const htmlString = `
     <div class="itemFromCollection">
       <div class="row">
         <div class="collectionHolder hitem">${el.collection}</div>
@@ -63,27 +67,27 @@ function renderItem() {
 }
 
 function initEvents() {
-  document.addEventListener('keydown', function(event) {
+  document.addEventListener('keydown', function (event) {
     // n = 78
     // space = 32
     // y = 89
     event.preventDefault();
-    if(event.keyCode == 32) {
+    if (event.keyCode == 32) {
       scrolling = false;
       const img = document.getElementById("currentImage");
       img.src = imageURIGenerator(currentlyWatching.gentImageURI, true)
     }
-    if(event.keyCode == 89) {
+    if (event.keyCode == 89) {
       vote(true);
       workerHasWorked++;
       document.getElementById("workerAmountHolder").innerHTML = workerHasWorked;
     }
-    if(event.keyCode == 78) {
+    if (event.keyCode == 78) {
       vote(false);
       workerHasWorked++;
       document.getElementById("workerAmountHolder").innerHTML = workerHasWorked;
     }
-    if(event.keyCode == 90) {
+    if (event.keyCode == 90) {
       currentWorker = generateUUID();
       workerHasWorked = 0;
 
@@ -94,21 +98,22 @@ function initEvents() {
   });
 
 }
+
 function vote(approved) {
 
   fetch("https://api.datacratie.cc/approvals", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ 
-      originID: currentlyWatching.originID, 
-      annotationUUID: currentlyWatching.id, 
-      workerID: currentWorker, 
-      collection: currentlyWatching.collection, 
-      approved: approved
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        originID: currentlyWatching.originID,
+        annotationUUID: currentlyWatching.id,
+        workerID: currentWorker,
+        collection: currentlyWatching.collection,
+        approved: approved
+      })
     })
-  })
     .then(r => r.json())
     .then((data) => {
       console.log(data)
@@ -121,24 +126,25 @@ function vote(approved) {
       renderLoop();
     })
 }
+
 function generateUUID() { // Public Domain/MIT
-  var d = new Date().getTime();//Timestamp
-  var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16;//random number between 0 and 16
-      if(d > 0){//Use timestamp until depleted
-          r = (d + r)%16 | 0;
-          d = Math.floor(d/16);
-      } else {//Use microseconds since page-load if supported
-          r = (d2 + r)%16 | 0;
-          d2 = Math.floor(d2/16);
-      }
-      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  var d = new Date().getTime(); //Timestamp
+  var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now() * 1000)) || 0; //Time in microseconds since page-load or 0 if unsupported
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16; //random number between 0 and 16
+    if (d > 0) { //Use timestamp until depleted
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else { //Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
   });
 }
 
 function imageURIGenerator(gentImageURI, override = false) {
-  if(USE_SMALL_IMAGES && !override) {
+  if (USE_SMALL_IMAGES && !override) {
     return `https://media.datacratie.cc/pictograms/${gentImageURI}`
   }
   return `https://api.collectie.gent/iiif/imageiiif/3/${gentImageURI}/full/^1000,/0/default.jpg`
